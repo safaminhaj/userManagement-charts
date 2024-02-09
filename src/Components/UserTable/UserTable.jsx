e9c6e9d5261ae6c9930ce516aef6159707227a20
safaminhaj <safaminhaj11@gmail.com>
2024-02-09 23:23:18 +0530

    toolbar added

--- src/Components/UserTable/UserTable.jsx
+++ src/Components/UserTable/UserTable.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Toolbar } from "primereact/toolbar";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
 import "primereact/resources/themes/saga-blue/theme.css";
+import "/node_modules/primeflex/primeflex.css";
+
+import "primeicons/primeicons.css";
 import "./UserTable.css";
 
 export const UserTable = () => {
@@ -64,6 +70,17 @@ export const UserTable = () => {
     },
   ];
 
+  const items = [
+    {
+      label: "Update",
+      icon: "pi pi-refresh",
+    },
+    {
+      label: "Delete",
+      icon: "pi pi-times",
+    },
+  ];
+
   const usernameBodyTemplate = (rowData) => {
     return (
       <div className="user-info">
@@ -81,8 +98,20 @@ export const UserTable = () => {
       </div>
     );
   };
+  const startContent = (
+    <React.Fragment>
+      <Button icon="pi pi-plus" className="mr-2" />
+    </React.Fragment>
+  );
+
+  const endContent = (
+    <span>
+      <InputText type="search" placeholder="Search" />
+    </span>
+  );
   return (
     <div className="user-table">
+      <Toolbar start={startContent} end={endContent} />
       <DataTable value={users}>
         <Column field="id" header="ID"></Column>
         <Column field="name" header="Name" sortable></Column>
